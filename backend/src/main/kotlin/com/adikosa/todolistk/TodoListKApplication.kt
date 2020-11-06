aff81a84f62547ee7269686688e9e8e993e4be6c
k0siara <patryk.kosieradzki@gmail.com>
2020-11-06 13:23:42 +0100

    Initial commit

---
+++ backend/src/main/kotlin/com/adikosa/todolistk/TodoListKApplication.kt
@@ -0,0 +1,11 @@
+package com.adikosa.todolistk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoListKApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodoListKApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/adikosa/todolistk/TodoListKApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.adikosa.todolistk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoListKApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
