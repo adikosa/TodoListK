4ea8361619f3d9077a873d96b81d24b3e77097fc
k0siara <patryk.kosieradzki@gmail.com>
2020-11-07 23:09:03 +0100

    Adding LoginPage and request handling

--- frontend/src/components/wrappers/App.js
+++ frontend/src/components/wrappers/App.js
@@ -1,11 +1,14 @@
-import React, {Component} from 'react';
-import TodoList from '../ui/TodoList';
-import StateProvider from './StateProvider';
-import KeyStrokeHandler from './KeyStrokeHandler';
+import React from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import * as TodoRepository from '../../data/repos/TodoRepository';
+import {PrivateRoute} from "../PrivateRoute";
+import {LoginPage} from "../../LoginPage";
+import { createBrowserHistory } from 'history';
+
+export const history = createBrowserHistory();
 
 
-class App extends Component {
+class App extends React.Component {
     render() {
         TodoRepository.getById(1).then((response) => {
             console.log(response.status)
@@ -18,11 +21,20 @@ class App extends Component {
             console.log(response.data);
         })
         return (
-            <StateProvider>
-                <KeyStrokeHandler>
-                    <TodoList/>
-                </KeyStrokeHandler>
-            </StateProvider>
+            <div className="jumbotron">
+                <div className="container">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        <Router history={history}>
+                            <Switch>
+                                {/*<PrivateRoute exact path="/" component={HomePage} />*/}
+                                <Route path="/login" component={LoginPage} />
+                                {/*<Route path="/register" component={RegisterPage} />*/}
+                                <Redirect from="*" to="/" />
+                            </Switch>
+                        </Router>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
---
+++ frontend/webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        config: JSON.stringify({
+            BASE_API_URL: 'http://localhost:8080/api'
+        })
+    }
+}
