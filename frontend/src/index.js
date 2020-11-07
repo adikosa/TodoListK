ec36d378885d4be2f3c0ef6d5effe164cb9ec387
Adrian <adriantwarowski1@gmail.com>
2020-11-07 13:54:58 +0100

    todo layout and TodoRepo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/wrappers/App';
 import reportWebVitals from './reportWebVitals';
+
+// Add bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+
+// Add our style
+import './assets/style/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
