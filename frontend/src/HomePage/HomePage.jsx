c99423ecb9c8fe3857029e15b000b8a10e806cc1
Adrian <adriantwarowski1@gmail.com>
2020-11-20 12:36:44 +0100

    css and css loaders in home page

--- frontend/src/HomePage/HomePage.jsx
+++ frontend/src/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import StateProvider from "../components/wrappers/StateProvider";
 import KeyStrokeHandler from "../components/wrappers/KeyStrokeHandler";
 import TodoList from "../components/ui/TodoList";
+import '../assets/style/index.css';
 
 class HomePage extends React.Component {
     render() {
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -10,7 +10,17 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 loader: 'babel-loader'
-            }
+            },
+            {
+                // Preprocess your css files
+                // you can add additional loaders here (e.g. sass/less etc.)
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /.(jpg|jpeg|png|svg)$/,
+                use: ['file-loader'],
+            },
         ]
     },
     plugins: [new HtmlWebpackPlugin({
