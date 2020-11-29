dfe550666b617b4965df03422a8148d60cf74a65
Adrian <adriantwarowski1@gmail.com>
2020-11-29 20:55:45 +0100

    Almost all modal is populated with received data. Issues with: due time, priority

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -8,8 +8,49 @@ import { getTodos } from '../store/actions/todoActions';
 import TodoModal from './TodoModal';
 
 class Home extends React.Component {
+    state = {
+        todoModalData : {
+            id: null,
+            title: "",
+            description: "",
+            priority: "",
+            dueDateTime: new Date()
+        },
+        isModalOpen : false
+    }
+
     componentDidMount(){
         this.props.getTodos()
+    }
+
+    handleEditClick = (todo) => {
+        this.setState({
+            todoModalData: todo
+        } , () => this.setState({
+            isModalOpen: true
+        }))
+    }
+
+    handleAddClick = () => {
+        const emptyTodoModalData = {
+            id: null,
+            title: "",
+            description: "",
+            priority: "MEDIUM",
+            dueDateTime: new Date()
+        }
+
+        this.setState({
+            todoModalData: emptyTodoModalData
+        } , () => this.setState({
+            isModalOpen: true
+        }))    
+    }
+
+    handleModalClose = () => {
+        this.setState({
+            isModalOpen: false
+        })
     }
     
     render() {
@@ -32,7 +73,7 @@ class Home extends React.Component {
                             </label>
                         </div>
                         <div className="col s1">
-                            <button data-target="TodoModal" className="btn-small modal-trigger red lighten-1">Edit
+                            <button onClick={() => this.handleEditClick(todo)} className="btn-small red lighten-1">Edit
                             </button>
                         </div>
                         <div className="col s1">
@@ -45,13 +86,16 @@ class Home extends React.Component {
             <div className="center">No todos yet</div>
         )
 
+    
+        const {id, title, description, priority, dueDateTime} = this.state.todoModalData
+
         return (
             <div className="container home">
-                <TodoModal id="TodoModal" title="Custom Edit Title" description="AAA" priority="MEDIUM" />
+                <TodoModal isOpen={this.state.isModalOpen} onClose={this.handleModalClose} id={id} title={title} description={description} priority={priority} dueDateTime={dueDateTime} />
                 <h5 className="grey-text text-darken-3">Tasks</h5>
                 {todoList}
                 <div className="fixed-action-btn">
-                    <button data-target="TodoModal" className="btn-floating modal-trigger btn-large red"><i
+                    <button onClick={this.handleAddClick}  className="btn-floating btn-large red"><i
                         className="material-icons">add</i>Add task
                     </button>
                 </div>
--- frontend/src/components/TodoModal.js
+++ frontend/src/components/TodoModal.js
@@ -1,32 +1,27 @@
 import React from "react";
 import M from "materialize-css";
-// import "materialize-css/dist/css/materialize.min.css";
-import {Button, DatePicker, Modal, TimePicker} from "react-materialize";
+import "materialize-css/dist/css/materialize.min.css";
+import {Button, DatePicker, Modal, Row, Select, TextInput, TimePicker} from "react-materialize";
 import { connect } from "react-redux";
 import {addTodo} from '../store/actions/todoActions'
 
 
 class TodoModal extends React.Component {
+    
     state = {
-        todo : {
-            title: this.props.title,
-            description: this.props.description,
-            dueDate: null,
-            dueTime: null,
-            priority: this.props.priority
-        }
+        title: this.props.description,
+        description: this.props.description,
+        dueDateTime: this.props.dueDateTime,
+        priority: this.props.priority
     }
 
     convertTodoStateToTodoRequest = () => {
-        const {title, description, dueDate, dueTime, priority} = this.state.todo;
-        var dueDateTime = new Date(dueDate.getTime())
-        dueDateTime.setHours(dueTime.hour)
-        dueDateTime.setMinutes(dueTime.minute)
+        const {title, description, dueDateTime, priority} = this.state;
         return {
             title,
             description,
-            priority,
-            dueDateTime
+            dueDateTime,
+            priority
         }
     }
 
@@ -39,18 +34,19 @@ class TodoModal extends React.Component {
         M.AutoInit()
     }
 
+    onOpenStart = () => {
+        const { title, description, dueDateTime, priority} = this.props;
+        this.setState({ title, description, dueDateTime, priority }, () => console.log("props received", this.state));
+    }
+
     handleClick = (e) => {
         this.props.addTodo(this.convertTodoStateToTodoRequest());
     }
 
     handleChange = (e) => {
         const {id, value} = e.target;
-        const {todo} = this.state;
         this.setState({
-            todo: {
-                ...todo,
-                [id]: value
-            }
+            [id]: value
         });
     }
 
@@ -62,50 +58,55 @@ class TodoModal extends React.Component {
         e.preventDefault()
     }
 
+    handleDateChange = (date) => {
+        const oldDueDateTime = this.state.dueDateTime
+        date.setHours(oldDueDateTime.getHours())
+        date.setMinutes(oldDueDateTime.getMinutes())
+        this.setState({
+            dueDateTime: date
+        })
+    }
+
     handleTimePickerClose = (e) => {
-        this.handleChange({target: {id: "dueTime", value: this.timePickerCachedData}})
+        const {dueDateTime} = this.state
+        dueDateTime.setHours(this.timePickerCachedData.hour)
+        dueDateTime.setMinutes(this.timePickerCachedData.minute)
+        this.setState({
+            dueDateTime
+        })    
     }
 
     render() {
         return (
             <div>
                 <Modal
+                    open={this.props.isOpen}
                     actions={[
                         <Button flat modal="close" node="button">Cancel</Button>,
                         <Button onClick={this.handleClick} flat modal="close" node="button">Save</Button>
                     ]}
 
-                    id={this.props.id}
+                    options={{onCloseEnd : this.props.onClose, onOpenStart: this.onOpenStart}}
                 >
                     <form onSubmit={this.handleSubmit} autoComplete="off">
-                        <div className="row">
-                            <div className="col s12 input-field">
-                                <input id="title" value={this.state.todo.title} type="text" onChange={this.handleChange}/>
-                                <label htmlFor="text">Title</label>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col s12 input-field">
-                                <input id ="description" value={this.state.todo.description} type="text" onChange={this.handleChange}/>
-                                <label htmlFor="text">Description</label>
-                            </div>
-                        </div>
-                        <div className="row">                           
-                            <DatePicker id="dueDate" label="Due date" options={{autoClose: true}} onChange={(date)=>this.handleChange({target: {id: "dueDate", value: date}})} />
-                            <TimePicker id="dueTime" label="Due time" options={{autoClose: true, twelveHour: false, onCloseEnd: this.handleTimePickerClose}} onChange={this.handleTime} />
-                        </div>
-                        <div className="row">
-                            <div className="col input-field">
-                                <select id="priority" value={this.state.todo.priority} onChange={this.handleChange}>
-                                    <option value="LOW">LOW</option>
-                                    <option value="MEDIUM">MEDIUM</option>
-                                    <option value="HIGH">HIGH</option>
-                                </select>
-                                <label>Priority</label>
-                            </div>
-                        </div>
+                        <Row>
+                            <TextInput id="title" label="Title" value={this.state.title} onChange={this.handleChange}/>  
+                        </Row>
+                        <Row>
+                            <TextInput id="description" label="Description" value={this.state.description} onChange={this.handleChange}/>  
+                        </Row>
+                        <Row>                           
+                            <DatePicker id="dueDate" label="Due date" options={{autoClose: true, defaultDate: this.state.dueDateTime, setDefaultDate: true}} onChange={this.handleDateChange} />
+                            <TimePicker id="dueTime" label="Due time" options={{autoClose: true, twelveHour: false, onCloseEnd: this.handleTimePickerClose, defaultTime: this.state.dueDateTime.getHours() + ":" + this.state.dueDateTime.getMinutes}} onChange={this.handleTime} />
+                        </Row>
+                        <Row>
+                            <Select id="priority" label="Priority" value={this.state.priority} onChange={this.handleChange}>
+                                <option value="LOW">LOW</option>
+                                <option value="MEDIUM">MEDIUM</option>
+                                <option value="HIGH">HIGH</option>
+                            </Select>
+                        </Row>
                     </form>
-                    
                 </Modal>
             </div>
         );
